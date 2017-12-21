587e94214b81c6d600a1ea2821eaf2c041cdb470
Kagami <moe.pascal@gmail.com>
2017-12-21 15:14:28 +0800

    update dep

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: 'Gatsby Default Starter',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-react-next',
+  ],
 }
