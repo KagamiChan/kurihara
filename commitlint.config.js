3becb684a4aacf514dea469b6f157f6b3216bc5b
Kagami <moe.pascal@gmail.com>
2018-10-10 12:04:45 +0800

    chore: add commit type for contents

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const { get } = require('lodash')
+const conventional = require('@commitlint/config-conventional')
+
+const types = get(conventional, ['rules', 'type-enum', 2], [])
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', types.concat('content')],
+  },
 }
