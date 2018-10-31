56d5b3fd032cb59fd147d80d112d768dac5ba2b9
Kagami <moe.pascal@gmail.com>
2018-10-31 18:22:40 +0800

    feat: reduce header height

--- src/components/base-layout.js
+++ src/components/base-layout.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { StaticQuery, graphql } from 'gatsby'
-import { rhythm } from '../utils/typography'
 
+import { rhythm } from '../utils/typography'
 import { theme } from '../utils/style'
 import blogLogo from '../assets/blog-logo.png'
 import 'prismjs/themes/prism.css'
@@ -24,7 +24,7 @@ const HeaderWrap = styled.div`
 const Header = styled.div`
   margin: 0 0 0 10%;
   max-width: 960px;
-  padding: ${rhythm(1)} 0;
+  padding: ${rhythm(1 / 3)} 0;
   display: flex;
   align-items: center;
 `
@@ -58,47 +58,55 @@ const Content = styled.div`
   padding-top: 0;
 `
 
-const BaseLayout = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <div>
-      <GlobalStyle />
-      <StaticQuery
-        query={graphql`
-          query LayoutQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
-          }
-        `}
-        render={data => (
-          <Helmet>
-            <title>{data.site.siteMetadata.title}</title>
-            <meta name="description" content="明镜止水的个人日志" />
-            <meta name="keywords" content="明镜止水, HP, BLOG" />
-            <script src="//fonts.gstatic.com/ea/timing/v1/mlfont.js" async />
-          </Helmet>
-        )}
-      />
-      <HeaderWrap>
-        <Header>
-          <H1>
-            <H1Link to="/blog">明镜止水</H1Link>
-          </H1>
-          <Nav />
-        </Header>
-      </HeaderWrap>
-      <Content>{children}</Content>
-    </div>
-  </ThemeProvider>
-)
+class BaseLayout extends Component {
+  static propTypes = {
+    data: PropTypes.shape({
+      site: PropTypes.object,
+    }).isRequired,
+    children: PropTypes.element.isRequired,
+  }
 
-BaseLayout.propTypes = {
-  data: PropTypes.shape({
-    site: PropTypes.object,
-  }).isRequired,
-  children: PropTypes.element.isRequired,
+  render() {
+    const { children } = this.props
+    return (
+      <ThemeProvider theme={theme}>
+        <div>
+          <GlobalStyle />
+          <StaticQuery
+            query={graphql`
+              query LayoutQuery {
+                site {
+                  siteMetadata {
+                    title
+                  }
+                }
+              }
+            `}
+            render={data => (
+              <Helmet>
+                <title>{data.site.siteMetadata.title}</title>
+                <meta name="description" content="明镜止水的个人日志" />
+                <meta name="keywords" content="明镜止水, HP, BLOG" />
+                <script
+                  src="//fonts.gstatic.com/ea/timing/v1/mlfont.js"
+                  async
+                />
+              </Helmet>
+            )}
+          />
+          <HeaderWrap>
+            <Header>
+              <H1>
+                <H1Link to="/blog">明镜止水</H1Link>
+              </H1>
+              <Nav />
+            </Header>
+          </HeaderWrap>
+          <Content>{children}</Content>
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
 
 export default BaseLayout
