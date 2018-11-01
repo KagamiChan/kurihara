48a04489331cc6fc43e1bd98249a3c21134e0fef
Kagami <moe.pascal@gmail.com>
2018-11-01 21:14:53 +0800

    feat: use react fontawesome

--- src/components/shortcut.js
+++ src/components/shortcut.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { rgba } from 'polished'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons/faArrowUp'
+
 import { rhythm } from '../utils/typography'
 
 const Sentinel = styled.div`
@@ -27,10 +30,6 @@ const Button = styled.a`
 
   :hover {
     background-color: ${props => rgba(props.theme.blue, 1)};
-  }
-
-  svg {
-    width: 0.875em;
   }
 `
 
@@ -68,17 +67,7 @@ class Shortcut extends Component {
       <>
         <Sentinel ref={this.sentinel} />
         <Button visible={visible} href="#blog-title" title="回到顶部">
-          <svg
-            aria-hidden="true"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path
-              fill="currentColor"
-              d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
-            />
-          </svg>
+          <FontAwesomeIcon icon={faArrowUp} />
         </Button>
       </>
     )
