6aef932b82e5e7d420784079b780bee3b2256644
Kagami <moe.pascal@gmail.com>
2018-11-08 03:45:06 +0800

    feat: rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,8 @@ const path = require('path')
 module.exports = {
   siteMetadata: {
     title: '少年读书隙中窥月',
+    description: '明镜止水的个人日志',
+    siteUrl: 'https://kagami.moe',
   },
   plugins: [
     'gatsby-plugin-lodash',
@@ -44,6 +46,70 @@ module.exports = {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) =>
+              allMarkdownRemark.edges.map(edge => ({
+                ...edge.node.frontmatter,
+                description: edge.node.excerpt,
+                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                custom_elements: [
+                  {
+                    'content:encoded': edge.node.html,
+                  },
+                ],
+              })),
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 20,
+                  sort: { order: DESC, fields: [frontmatter___publish_date] },
+                  filter: {
+                    frontmatter: {
+                      draft: { ne: true }
+                    },
+                    fields: {
+                      type: {eq: "blog"}
+                    }
+                  }
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields {
+                        slug
+                      }
+                      frontmatter {
+                        title
+                        date: publish_date(formatString: "")
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/feed.xml',
+            title: 'RSS Feed',
+          },
+        ],
       },
     },
     'gatsby-plugin-netlify',
