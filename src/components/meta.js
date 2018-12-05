b0179b45e77f39bdb078680e51b987ef2d41039b
Kagami <moe.pascal@gmail.com>
2018-12-06 02:19:41 +0800

    feat: i18n for footer

--- src/components/meta.js
+++ src/components/meta.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { rgba } from 'polished'
-import { withNamespaces } from 'react-i18next'
+import { withNamespaces, Trans } from 'react-i18next'
 import { map, find } from 'lodash'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLanguage } from '@fortawesome/free-solid-svg-icons/faLanguage'
@@ -99,25 +99,23 @@ const LanguageSwitch = withNamespaces()(
   },
 )
 
-const Comment = () => (
+/* eslint-disable prettier/prettier */
+const Comment = withNamespaces()(() => (
   <Wrapper>
     <LanguageSwitch />
-    自豪地基于 <a href="https://reactjs.org">React.js</a> 与{' '}
-    <a href="https://gatsbyjs.org">Gatsby.js</a> 驱动 | 托管于 Netlify |{' '}
-    <a href="/rss.xml">RSS 订阅可用</a>
-    <br />
-    内容基于{' '}
-    <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode">
-      CC-BY-SA 4.0
-    </a>{' '}
-    授权。评点或斧正可以
-    <a href="https://github.com/KagamiChan/kurihara/issues/new">
-      在此提交 issue
-    </a>
-    。<br />
-    2013-
-    {nowYear} 鏡 ＠ がんばらないプロジェクト / 夜ノ森工房
+    <div>
+      <Trans i18nKey="ui:meta_first_line">
+        自豪地基于 <a href="https://reactjs.org">React.js</a> 与 <a href="https://gatsbyjs.org">Gatsby.js</a> 驱动 | 托管于 Netlify | <a href="/rss.xml">RSS 订阅可用</a>
+      </Trans>
+    </div>
+    <div>
+      <Trans i18nKey="ui:meta_second_line">
+        内容基于 <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode">CC-BY-SA 4.0</a> 授权 | 评点或斧正可以 <a href="https://github.com/KagamiChan/kurihara/issues/new"> 在此提交 issue </a>
+      </Trans>
+    </div>
+    <div>2013-{nowYear} 鏡 ＠ がんばらないプロジェクト / 夜ノ森工房</div>
   </Wrapper>
-)
+))
+/* eslint-enable prettier/prettier */
 
 export default Comment
