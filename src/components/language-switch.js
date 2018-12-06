c64999140f8af84211e111909bf56c3aaa8bd6fa
Kagami <moe.pascal@gmail.com>
2018-12-06 15:07:01 +0800

    feat: language loading indicator

--- src/components/language-switch.js
+++ src/components/language-switch.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { rgba } from 'polished'
 import { withNamespaces } from 'react-i18next'
 import { map, find } from 'lodash'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLanguage } from '@fortawesome/free-solid-svg-icons/faLanguage'
+import { faHourglass } from '@fortawesome/free-solid-svg-icons/faHourglass'
 import { Popover, Position } from '@blueprintjs/core'
 
 import { rhythm } from '../utils/typography'
@@ -33,27 +35,30 @@ const Switches = styled.div`
 `
 
 const Switch = styled.div`
-  margin: ${rhythm(0.5)};
+  padding: ${rhythm(0.25)};
+  margin: ${rhythm(0.25)};
   cursor: pointer;
 
   color: ${props => props.active && props.theme.blue};
+  background-color: ${props => props.active && rgba(props.theme.blue, 0.1)};
 `
 
 const LangugeIndicator = styled(Popover)`
   cursor: pointer;
 `
 
-const LanguageSwitch = withNamespaces()(
+const LanguageSwitch = withNamespaces([], { wait: false })(
   class LanguageSwitch extends Component {
     static propTypes = {
       i18n: PropTypes.shape({
         changeLanguage: PropTypes.func,
         language: PropTypes.string,
       }).isRequired,
+      tReady: PropTypes.bool.isRequired,
     }
 
     render() {
-      const { i18n } = this.props
+      const { i18n, tReady } = this.props
       const currentLanguage = find(LANGUAGES, ({ value }) =>
         i18n.language.startsWith(value),
       )?.display
@@ -64,7 +69,8 @@ const LanguageSwitch = withNamespaces()(
           wrapperTagName="div"
         >
           <div>
-            <FontAwesomeIcon icon={faLanguage} /> {currentLanguage}
+            <FontAwesomeIcon icon={tReady ? faLanguage : faHourglass} />{' '}
+            {currentLanguage}
           </div>
           <Switches>
             {map(LANGUAGES, ({ display, value }) => (
