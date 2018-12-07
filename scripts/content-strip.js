d63fcb2a4af4da891a6150e35be9a7b569f408a0
Kagami <moe.pascal@gmail.com>
2018-12-07 14:45:39 +0800

    feat: strip unexpected backspace char

---
+++ scripts/content-strip.js
@@ -0,0 +1,21 @@
+const replaceInFile = require('replace-in-file')
+
+const files = process.argv.slice(2)
+
+/**
+ * Squirrel could accidently leave backspace character (u0008) in markdown contents, breaking browser rss renderer
+ */
+const main = async () => {
+  try {
+    await replaceInFile({
+      files,
+      from: [/\u0008/g], // eslint-disable-line no-control-regex
+      to: '',
+    })
+  } catch (e) {
+    console.error(e)
+    process.exitCode = 1
+  }
+}
+
+main()
