e26bb3aa9f0e92e3f616d0e7c1b9090c4b5fbaf1
Kagami <moe.pascal@gmail.com>
2020-02-22 21:37:18 +0800

    fix: repo link

--- scripts/release.ts
+++ scripts/release.ts
@@ -9,22 +9,23 @@ declare module 'gh-pages' {
 
 const main = async () => {
   try {
-    if (!process.env.GITHUB_TOKEN) {
+    const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_ACTOR } = process.env
+    if (!GITHUB_TOKEN) {
       throw new Error('No token specified.')
     }
 
-    if (!process.env.GITHUB_REPOSITORY) {
+    if (!GITHUB_REPOSITORY) {
       throw new Error(
         'No repository info detected, this script is expected to run in GitHub actions.',
       )
     }
 
-    console.info(`Deploying to ${process.env.GITHUB_REPOSITORY}`)
+    console.info(`Deploying to ${GITHUB_REPOSITORY}`)
 
     await ghpages.publish(path.resolve(__dirname, '../public'), {
       branch: 'release',
       silent: true,
-      repo: `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
+      repo: `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`,
       dotfiles: true,
       history: false,
       user: {
